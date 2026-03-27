28b154f985dbb016f8620a24ae7bae406b3a00c5
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-03-27 22:37:37 +0530

    Initial commit without large dataset

---
+++ diag.bat
@@ -0,0 +1,3 @@
+git status > "b:\Predicting-Residential-Electricity-Consumption-Using-CNN-BiLSTM-SA-Neural-Networks\status.txt" 2>&1
+git log --oneline -n 5 >> "b:\Predicting-Residential-Electricity-Consumption-Using-CNN-BiLSTM-SA-Neural-Networks\status.txt" 2>&1
+git remote -v >> "b:\Predicting-Residential-Electricity-Consumption-Using-CNN-BiLSTM-SA-Neural-Networks\status.txt" 2>&1
